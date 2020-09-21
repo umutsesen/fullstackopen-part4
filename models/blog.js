c18726d07796e857d58a99af8db96ab90f1ea0e8
umutsesen <umutsesen99@gmail.com>
2020-09-21 19:54:59 +0300

    Initial commit

---
+++ models/blog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+blogSchema.plugin(uniqueValidator)
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+
+module.exports = mongoose.model('Blog', blogSchema)
---
+++ utils/logger.js
@@ -0,0 +1,13 @@
+
+const error = (...params) => {
+  console.log(...params)
+}
+const info = (...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
+}
+
+
+
+module.exports = { error, info }
